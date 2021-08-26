b0705127ca011d4351a18c65af635e90bb9e58ca
PerseveringLumberJack <873298352@qq.com>
2021-08-26 18:20:23 +0800

    pom

---
+++ src/test/java/com/example/springcloud/eureka/SpringCloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
